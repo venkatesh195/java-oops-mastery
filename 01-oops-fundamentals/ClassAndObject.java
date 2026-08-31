cf2afe57fa400583d6656681d8ba8fe8769aa1d6
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-08-31 13:03:12 +0530

    Add Student class and main method for demonstration

--- 01-oops-fundamentals/ClassAndObject.java
+++ 01-oops-fundamentals/ClassAndObject.java
@@ -1,1 +1,19 @@
+class Student {
 
+    String name;
+    int age;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Venkatesh";
+        s1.age = 23;
+
+        System.out.println("Name: " + s1.name);
+        System.out.println("Age: " + s1.age);
+    }
+}
