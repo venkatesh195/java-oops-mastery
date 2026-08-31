b3f0640ef44bdca0074e9600edc37d24efd16c33
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-08-31 13:09:17 +0530

    Concept: Two references pointing to two different objects

---
+++ 01-oops-fundamentals/ObjectReference.java
@@ -0,0 +1,24 @@
+class Student {
+
+    String name;
+    int age;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Venkatesh";
+        s1.age = 23;
+
+        Student s2 = new Student();
+
+        s2.name = "Venky";
+        s2.age = 21;
+
+        System.out.println("Student 1: " + s1.name);
+        System.out.println("Student 2: " + s2.name);
+    }
+}
