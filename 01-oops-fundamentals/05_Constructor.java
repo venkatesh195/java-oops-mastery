57f286384ea83e8d30093fb9c0e003d081442006
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-08-31 13:19:39 +0530

    Concept: Constructor and no-argument constructor
    Removed student name and age assignments from main method.

---
+++ 01-oops-fundamentals/05_Constructor.java
@@ -0,0 +1,17 @@
+class Student {
+
+    String name;
+    int age;
+
+    Student() {
+        System.out.println("Constructor called");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+    }
+}
