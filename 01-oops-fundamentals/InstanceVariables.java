a3fc784ade787576e1db4297d70ebb12e0e506d9
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-08-31 13:04:36 +0530

    Add Student class with instance variables and main method

---
+++ 01-oops-fundamentals/InstanceVariables.java
@@ -0,0 +1,25 @@
+class Student {
+
+    String name;
+    int age;
+    String course;
+    float marks;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Venkatesh";
+        s1.age = 23;
+        s1.course = "B.Tech";
+        s1.marks = 99.9f;
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.course);
+        System.out.println(s1.marks);
+    }
+}
