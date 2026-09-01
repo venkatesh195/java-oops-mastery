b7470dbdc3c2a539592f3597ed8cab78883aed31
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-09-01 15:30:15 +0530

    Implement Super Keyword in OOP Example

---
+++ 02_The Four Pillars of OOP/05_SuperKeyword.java
@@ -0,0 +1,79 @@
+//-------------------------------------Parent variable-----------------------------------//
+import java.util.*;
+class Person{
+  String name="john Wick";
+
+  public void displayPerson(){
+    System.out.println("=======Displaying Person detail's=========");
+    System.out.println("Person Name :- "+name);
+    
+  }
+}
+class Employee extends Person{
+  String name="venkatesh";
+
+  public void displayEmployee(){
+    System.out.println("=======Displaying Employee detail's=========");
+    System.out.println("employee name :- "+name);
+    System.out.println("Person Name :- "+super.name);
+  }
+
+}
+public class Main {
+    public static void main(String[] args) {
+      Employee mg=new Employee();
+      mg.displayEmployee();
+    }
+}
+/--------------------------------Parent method-----------------------------------//
+import java.util.*;
+class Person{
+  public void display(){
+    System.out.println("Person ");
+    
+  }
+}
+class Employee extends Person{
+
+  public void display(){
+    System.out.println("employee ");
+  }
+  public void ShowBoth(){
+    display();
+    super.display();
+  }
+
+}
+public class Main {
+    public static void main(String[] args) {
+      Employee mg=new Employee();
+      mg.ShowBoth();
+    }
+}
+//----------------------Parent constructor --------------------------------//
+class Person {
+    String name;
+    int age;
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+class Employee extends Person {
+    int employeeId;
+    Employee(String name, int age, int employeeId) {
+        super(name, age);
+        this.employeeId = employeeId;
+    }
+    void display() {
+        System.out.println("Name :- " + name);
+        System.out.println("Age :- " + age);
+        System.out.println("Employee Id :- " + employeeId);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Employee mg = new Employee("Venkatesh", 23, 101);
+        mg.display();
+    }
+}
