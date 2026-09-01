b7e9b12612450981b449db747d7b0286ef0f320d
VENKATESH PUTLA <143725992+venkatesh195@users.noreply.github.com>
2026-09-01 21:52:12 +0530

    Implement payment system with multiple payment methods

---
+++ 02_The Four Pillars of OOP/Payment_System.java
@@ -0,0 +1,41 @@
+interface Payment{
+  public void pay();
+  default void receipt(){
+    System.out.println("Receipt generated");
+  }
+  static void info(){
+    System.out.println("Payment information is here");
+  }
+}
+class UPIPayment implements Payment{
+  @Override
+  public void pay(){
+    System.out.println("UPIPayment payment successful");
+  }
+}
+class PhonePePayment extends UPIPayment{
+  @Override
+  public void pay(){
+    System.out.println("PhonePe payment successful");
+  }
+}
+class CardPayment implements Payment{
+  @Override
+  public void pay(){
+    System.out.println("CardPayment");
+  }
+}
+public class Main{
+  public static void main(String[] args){
+    Payment p1 = new PhonePePayment();
+    Payment p2 = new CardPayment();
+
+    p1.pay();
+    p1.receipt();
+
+    p2.pay();
+    p2.receipt();
+
+    Payment.info();
+  }
+}
